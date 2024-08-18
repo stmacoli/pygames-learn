58f61474f44f114338656915ca7869eeeaf98484
StMacoli <st.macoli@gmail.com>
2024-08-17 22:37:28 -0300

    commit2

--- code/main.py
+++ code/main.py
@@ -17,6 +17,7 @@ x = 10
 # importing an image
 player_surf = py.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = 1
 
 star_surf = py.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT))for i in range(20)]
@@ -25,7 +26,7 @@ meteor_surf = py.image.load(join('images', 'meteor.png')).convert_alpha()
 meteor_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 laser_surf = py.image.load(join('images', 'laser.png')).convert_alpha()
-laser_rect = player_surf.get_frect(bottomleft = (1260, 700))
+laser_rect = player_surf.get_frect(bottomleft = (20, 700))
 
 while running:
     # event loop
@@ -37,13 +38,21 @@ while running:
     display_surface.fill('darkgrey')
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
+
     display_surface.blit(meteor_surf, meteor_rect)
+
     display_surface.blit(laser_surf, laser_rect)
-    if player_rect.right < WINDOW_WIDTH :    
-        player_rect.left += 0.2
+
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+
+    #if player_rect.right < WINDOW_WIDTH :    
+     #   player_rect.left += 0.2
         
-    elif player_rect.right >= WINDOW_WIDTH :
-        player_rect.left -= 0.8
+    #elif player_rect.right >= WINDOW_WIDTH :
+     #   player_rect.left -= 0.8
     display_surface.blit(player_surf, player_rect)
 
 
